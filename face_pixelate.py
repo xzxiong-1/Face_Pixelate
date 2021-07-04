9f1d60e7e4df748c3ae566496962e82f091e933d
xzxiong-1 <67236852+xzxiong-1@users.noreply.github.com>
2021-07-04 16:19:37 +0800

    Update face_pixelate.py

--- face_pixelate.py
+++ face_pixelate.py
@@ -16,11 +16,11 @@ def vis_detections(im, dets, thresh=0.5, show_text=True):
         
 
         #对face打马赛克
-        im = np.array(im)  #转化为numpy
+        im = np.array(im)  
         # print(im1.shape)
         depts=10
         #for i range(dest)
-            for i in range(int(bbox[1]),int(bbox[3]), depts): #range(a,b,c)表示从a到b 每次增加c
+            for i in range(int(bbox[1]),int(bbox[3]), depts): 
                 for j in range(int(bbox[0]),int(bbox[2]), depts):
                     if i<h and j<w:
                         im[i:i + depts, j:j + depts] = im[i][j]
@@ -35,15 +35,15 @@ device = torch.device("cuda")
 conf_thresh = 0.3
 
 
-
 net = SSD("test")
 net.load_state_dict(torch.load('weights/WIDERFace_DSFD_RES152.pth'))
 net.to(device).eval();
 
 writeVideo_flag = True
-path="./008.mp4"
-cap = cv2.VideoCapture(path)
 
+# 视频输入
+path="./vedio_input/008.mp4"
+cap = cv2.VideoCapture(path)
 
 
 if writeVideo_flag:
@@ -53,7 +53,9 @@ if writeVideo_flag:
     fps = int(cap.get(5))
     target_size = (w, h)
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out= cv2.VideoWriter('./output008.mp4', fourcc, fps, (w, h))
+    
+    # 结果输出
+    out= cv2.VideoWriter('./vedio_result/output008.mp4', fourcc, fps, (w, h))
     while cap.isOpened():
 
         ret,frame=cap.read()
